830f6ef498432eeb587f09b85802fe4068c94be6
RAsya-satya <sae121222@gmail.com>
2025-03-04 13:35:02 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+let nama = 'RASYA' // tipe string
+let usia = 34 // tipe integer number
+let tingiBadan =170.3 // tipe double foat
+let  beratBadan
+let pacar =null
+
+
+alert(`nama saya ${nama} usia saya itu ${usia} tingi badan saya adalah 
+${tingiBadan} berat badan saya ${beratBadan} dan pacar saya ${pacar}`)
